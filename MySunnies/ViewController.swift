5a9b0301b2dd72bf04c4cc6d41e8fce4e1922e37
jjeui0308 <jjeui0308@gmail.com>
2017-06-28 12:52:00 +0900

    Initial Commit

---
+++ MySunnies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySunnies
+//
+//  Created by Justin Ji on 28/06/2017.
+//  Copyright © 2017 Justin Ji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
